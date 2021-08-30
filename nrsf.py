9f8e4f15dbd08c66e06e2c2e00109c6992d6acc9
Mikhail Yudin <fagci.nsk@gmail.com>
2021-08-31 00:40:12 +0700

    ftp

--- nrsf.py
+++ nrsf.py
@@ -55,7 +55,7 @@ def stalk(limit, workers, modules_to_load, debug=False):
     if handlers:
         print('Loaded handlers:')
         for h in handlers:
-            print('-', h.__class__.__name__, f'({h.PORT} port)')
+            print('-', h.__module__.split('.')[-1], f'({h.PORT} port)')
     else:
         print('No handlers loaded, exiting.')
         sys.exit()
