3349b65193269832205bd9cb8796a452bf2189a3
Mikhail Yudin <fagci.nsk@gmail.com>
2021-08-30 22:35:13 +0700

    html title

--- handlers/_http.py
+++
@@ -1,9 +0,0 @@
-from handlers import Base
-
-class Http(Base):
-    PORT = 80
-    def process(self, s):
-        ip, _ = s.getpeername()
-        s.send(f'GET / HTTP/1.1\r\nHost: {ip}\r\n\r\n'.encode())
-        return s.recv(1024).decode(errors='ignore')
-
