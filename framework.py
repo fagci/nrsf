97d25420fe16115442dcb0370a5fe58fb6cb1f07
Mikhail Yudin <fagci.nsk@gmail.com>
2021-09-02 01:45:49 +0700

    refactor

---
+++ framework.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from pkgutil import iter_modules
+import sys
+
+from lib.generators import generate_ips
+from lib.processors import Processor
+OUTPUT_PATH = Path(__file__).resolve().parent / 'out'
+
+class NRSF:
+    def __init__(self, modules_to_load, iface, debug, workers, limit):
+        self.handlers = []
+        self.proc = Processor()
+        self.workers = workers
+        self.limit = limit
+
+        for _, m, _ in iter_modules(['handlers']):
+            if m.startswith('_') or (m.lower() not in modules_to_load):
+                continue
+            module = getattr(__import__(f'handlers.{m}'), m)
+            handler = getattr(module, 'Handler')
+            handler.set_iface(iface)
+            handler.set_print_lock(self.proc.print_lock)
+            handler.set_output_path(OUTPUT_PATH)
+            if debug:
+                handler.DEBUG = True
+            self.handlers.append(handler)
+
+        if self.handlers:
+            print('Loaded handlers:')
+            for h in self.handlers:
+                print('-', h.get_name(), f'({h.PORT} port)')
+        else:
+            print('No handlers loaded, exiting.')
+            sys.exit()
+    def scan(self, ip_address, _):
+        for handler_class in self.handlers:
+            ip = str(ip_address)
+
+            with handler_class(ip) as handler:
+                handler.handle()
+
+    def run(self):
+        self.proc.process_each(self.scan, generate_ips(self.limit), self.workers)
+
--- nrsf.py
+++ nrsf.py
@@ -1,57 +1,17 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-from pathlib import Path
-from pkgutil import iter_modules
+from framework import NRSF
 from socket import setdefaulttimeout
-import sys
-
-from lib.generators import generate_ips
-from lib.processors import Processor
-
-OUTPUT_PATH = Path(__file__).resolve().parent / 'out'
 
 
-def scan(ip_address, _, handlers):
-    for handler_class in handlers:
-        ip = str(ip_address)
-
-        with handler_class(ip) as handler:
-            handler.handle()
-
-def bootstrap(modules_to_load, print_lock, iface, debug):
-    handlers = []
-
-    for _, m, _ in iter_modules(['handlers']):
-        if m.startswith('_') or (m.lower() not in modules_to_load):
-            continue
-        module = getattr(__import__(f'handlers.{m}'), m)
-        handler = getattr(module, 'Handler')
-        handler.set_iface(iface)
-        handler.set_print_lock(print_lock)
-        handler.set_output_path(OUTPUT_PATH)
-        if debug:
-            handler.DEBUG = True
-        handlers.append(handler)
-
-    if handlers:
-        print('Loaded handlers:')
-        for h in handlers:
-            print('-', h.get_name(), f'({h.PORT} port)')
-    else:
-        print('No handlers loaded, exiting.')
-        sys.exit()
-
-    return handlers
 
 
 def stalk(limit, workers, modules_to_load, debug=False, iface=''):
-    proc = Processor()
-    handlers = bootstrap(modules_to_load, proc.print_lock, iface, debug)
+    app = NRSF(modules_to_load,iface,debug,workers, limit)
 
     print('Stalking...', end='\n\n')
-
-    proc.process_each(scan, generate_ips(limit), workers, handlers)
+    app.run()
 
 
 if __name__ == '__main__':
