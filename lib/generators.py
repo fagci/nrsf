bdb049c55d5b92f5c215e03d82aa6d4253dd8156
Mikhail Yudin <fagci.nsk@gmail.com>
2021-09-04 15:22:41 +0700

    infinite generator

--- lib/generators.py
+++ lib/generators.py
@@ -7,7 +7,7 @@ def generate_ips(count=1000000, netork_range = ''):
         for host in IPv4Network(netork_range, strict=False).hosts():
             yield str(host)
         return
-    while count > 0:
+    while count != 0: # negative values makes infinite generator
         ip = IPv4Address(randrange(0, 0xffffffff))
         if ip.is_link_local or ip.is_loopback or ip.is_multicast or ip.is_private or ip.is_reserved:
             continue
