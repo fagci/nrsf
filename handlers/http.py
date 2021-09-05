a9cf269744fd340474706e08376d883b10aa8db2
Mikhail Yudin <fagci.nsk@gmail.com>
2021-09-05 16:48:22 +0700

    better response parsing

--- handlers/http.py
+++ handlers/http.py
@@ -3,17 +3,22 @@ from html.parser import HTMLParser
 
 class Handler(Base):
     PORT = 80
+
     def handle(self):
         return self.handle_host(self.ip)
 
     def handle_host(self, hostname):
-        html = self.dialog((
+        response = self.dialog((
             'GET / HTTP/1.1\r\n'
             f'Host: {hostname}\r\n'
             'User-Agent: Mozilla/5.0\r\n'
             'Accept: text/html\r\n'
             '\r\n'
-        ).encode(), 4096)
+        ).encode())
+        if '\r\n\r\n' not in response:
+            return
+        header, html = response.split('\r\n\r\n', 1)
+
         if self.DEBUG:
             with self._print_lock:
                 print(html)
@@ -28,7 +33,7 @@ class TitleParser(HTMLParser):
         self.title = ''
 
     def handle_starttag(self, tag, _):
-        self.match = tag == 'title'
+        self.match = tag == 'title' or tag == 'TITLE'
 
     def handle_data(self, data):
         if self.match:
