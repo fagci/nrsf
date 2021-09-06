1a0bcf6a032478f5bcd9bde2d3edb1b81dad6bd0
Mikhail Yudin <fagci.nsk@gmail.com>
2021-09-06 13:55:59 +0700

    better IP gen

--- lib/generators.py
+++ lib/generators.py
@@ -3,13 +3,15 @@ from random import randrange
 
 MAX_IPV4 = 1 << IPV4LENGTH
 
-def generate_ips(count=1000000, netork_range = ''):
+
+def generate_ips(count=1000000, netork_range=''):
     if netork_range:
         for host in IPv4Network(netork_range, strict=False).hosts():
             yield str(host)
         return
-    while count != 0: # negative values makes infinite generator
-        ip = IPv4Address(randrange(0, MAX_IPV4))
-        if ip.is_global and not ip.is_multicast:
+
+    while count:  # negative values makes infinite generator
+        ip_address = IPv4Address(randrange(0, MAX_IPV4))
+        if ip_address.is_global and not ip_address.is_multicast:
             count -= 1
-            yield str(ip)
+            yield str(ip_address)
